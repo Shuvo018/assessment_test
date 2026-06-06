aba3cfa28dcaeaffaecc31e541b925582c66777c
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-06-06 17:39:10 +0600

    Add static file settings and create style.css for consistent styling

--- main/settings.py
+++ main/settings.py
@@ -109,11 +109,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 # custom User model
 AUTH_USER_MODEL = "account_app.User"
