cd7935186607f93b0cacad3bebcc38bfc576d5cf
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-07-06 00:48:26 +0600

    try to deploy

--- main/settings.py
+++ main/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,9 +142,4 @@ LOGIN_REDIRECT_URL = "/auth/dashboard/"
 LOGOUT_REDIRECT_URL = "/auth/login/"
  
 
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
