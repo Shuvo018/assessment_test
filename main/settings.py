2914916b46758c4e06eea0401a93f9e330655eda
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-07-06 00:20:25 +0600

    try to fix port

--- main/settings.py
+++ main/settings.py
@@ -9,10 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(
-    "SECRET_KEY",
-    "django-insecure-so#xh(4w7*mz1hev%$tkrhc$zlqgtd*u+!b#ld6-343vb997xd"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
